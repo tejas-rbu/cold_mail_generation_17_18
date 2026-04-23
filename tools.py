cb38d9892541f139a1b70f5ed401ec16e69d774d
tejas-rbu <24ghonget@rbunagpur.in>
2026-04-23 23:00:31 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,27 @@
 import os
 from groq import Groq
+from tavily import TavilyClient
 
+
+# ---------------- GROQ ----------------
 def get_client():
-    return Groq(api_key=os.environ["GROQ_API_KEY"])
+    return Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+# ---------------- TAVILY ----------------
+def search_web(query):
+
+    client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+    response = client.search(
+        query=query,
+        search_depth="basic"
+    )
+
+    results = response.get("results", [])
+
+    context = "\n".join(
+        [f"{r.get('title')} - {r.get('content')}" for r in results[:5]]
+    )
+
+    return context
